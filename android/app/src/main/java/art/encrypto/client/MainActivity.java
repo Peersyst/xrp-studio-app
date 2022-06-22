3ac64af2178e04d85d860edae91e07d5380287ab
Michał Leszczyński <ml@icedev.pl>
2022-06-23 00:32:47 +0200

    rename package to art.encrypto.client

--- android/app/src/main/java/art/encrypto/client/MainActivity.java
+++ android/app/src/main/java/art/encrypto/client/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ntag5client;
+package art.encrypto.client;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "NTAG5Client";
+    return "Encrypto Art";
   }
 }
