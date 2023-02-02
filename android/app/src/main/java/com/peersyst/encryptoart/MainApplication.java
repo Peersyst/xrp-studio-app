8787c972b6302db55f082b5ad34e3293077a659e
Michał Leszczyński <ml@icedev.pl>
2023-02-02 11:51:25 +0000

    Fix for Android

--- android/app/src/main/java/com/peersyst/encryptoart/MainApplication.java
+++ android/app/src/main/java/com/peersyst/encryptoart/MainApplication.java
@@ -1,4 +1,4 @@
-package art.encrypto.client;
+package com.peersyst.encryptoart;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("art.encrypto.client.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.peersyst.encryptoart.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
